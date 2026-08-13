36e60992a91aae7df2d7f4923af3c25d684b0211
Henrique <jjazism@gmail.com>
2026-08-13 12:23:43 -0300

    exercicios JS

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+console.log("Bom dia!");
+var vel = 67
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+    if (vel > 67) { // condição simples
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);8
+    }
+console.log(`Dirija sempre usando cinto de segurança!`);
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+console.log("Bom Dia!!!");
+
+var pais = "EUA"
+console.log (`vivendo em ${pais}`);
+if (pais == "brasil") {
+    console.log("Você é brasileiro!");
+} else {
+    console.log("Você é estrangeiro!");
+}
