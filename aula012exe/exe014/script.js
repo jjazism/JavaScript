831e47001417c1be5151fc7bc2a49f610bb3aa82
Henrique <jjazism@gmail.com>
2026-08-18 12:15:37 -0300

    verificador de idade

--- aula012exe/exe014/script.js
+++ aula012exe/exe014/script.js
@@ -1,1 +1,51 @@
-alert ("ola")
+function verificar(){
+    var data = new Date() 
+      var ano = data.getFullYear()
+      var fano = document.getElementById(`txtano`)
+      var res = document.querySelector(`div#res`)
+
+      if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert(`[ERROR]`)
+      } else{
+        var fsex = document.getElementsByName (`radsex`)
+        var idade = ano - Number(fano.value)
+        var genero = ``
+        var img = document.createElement(`img`)
+        img.setAttribute(`id`, `foto`)
+        if(fsex[0].checked){
+            genero = `Homem`
+            if (idade >= 0 && idade <10){
+              // Crianca
+              img.setAttribute(`src`, `H-bebe.png`)
+            } else if (idade < 21){
+              // Jovem
+              img.setAttribute(`src`, `M-jovem.png`)
+            } else if (idade < 50){
+              // adulto
+              img.setAttribute(`src`, `H-adulto.png`)
+            } else {
+              //idoso
+              img.setAttribute(`src`, `H-idoso.png`)
+            }
+
+        } else if (fsex[1].checked){
+            genero = `Mulher`
+            if (idade >= 0 && idade <10){
+              // Crianca
+              img.setAttribute(`src`, `F-bebe.png`)
+            } else if (idade < 21){
+              // Jovem
+              img.setAttribute(`src`, `F-jovem.png`)
+            } else if (idade < 50){
+              // adulto
+              img.setAttribute(`src`, `F-adulto.png`)
+            } else {
+              //idoso
+              img.setAttribute(`src`, `F-idosa.png`)
+            }
+        }
+        res.style.textAlign = `center`
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+      }
+}
