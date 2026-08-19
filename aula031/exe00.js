fe3a15c27021293769f9e7383b882ef2e470b715
Henrique <jjazism@gmail.com>
2026-08-19 10:49:18 -0300

    exe
    loopings / repetições
    exe simples

---
+++ aula031/exe00.js
@@ -0,0 +1,28 @@
+var c = 1
+while (c <= 10){ // Enquanto c for MENOR que 10 faça isso
+    console.log(`contador ${c}`)
+    c++
+}
+
+/*
+do { // Faça isso enquanto c for MENOR que 10
+    console.log(`contador $(c)`)
+    c++
+} while (c <=10)
+
+
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+console.log(`contador`)
+*/
+
+
+
