64d06c759fec1b9fba57a30d498548e3187686fa
Henrique <jjazism@gmail.com>
2026-08-24 12:33:50 -0300

    arrays
    "Um array é uma variável composta que tem vários elementos. Cada elemento é composto por seu valor e por uma chave (key) de identificação".

---
+++ aula015/ambiente.js
@@ -0,0 +1,18 @@
+let num = [2,3,4,5,7]
+num[3] = 9
+num.push(1)
+num.push(10)
+num.sort()
+console.log (num)
+console.log (`vetor na segunda posição é ${num[2]}`)
+console.log(`o vetor tem ${num.length} posições`)
+
+let pos = num.indexOf(2)
+
+if(pos == -1){
+    console.log("numero nao encontrado")
+} else{
+    console.log(`Numero  ${pos} encontrado`)
+}
+
+//"Um array é uma variável composta que tem vários elementos. Cada elemento é composto por seu valor e por uma chave (key) de identificação". 
---
+++ aula015/valores.js
@@ -0,0 +1,11 @@
+let valores =[10,50,200,100,45,20]
+/*
+console.log (valores)
+
+for(let pos=0; pos < valores.length; pos++){
+    console.log(valores[pos])
+} */
+
+for( let pos in valores){
+    console.log(valores[pos])
+}
