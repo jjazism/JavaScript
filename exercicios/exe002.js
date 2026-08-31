e528958a54bd862be1a4cef2d609aa07ea68210a
Henrique <jjazism@gmail.com>
2026-08-31 14:17:35 -0300

    exe
    alguns exe

---
+++ exercicios/exe002.js
@@ -0,0 +1,15 @@
+let num = [2,4,1,5,8,3,7]
+num.push(80)
+
+for(let pos=0;pos<num.length;pos++){
+    console.log(`posicao ${pos} tem o valor ${num[pos]}`)
+}
+console.log(`existem ${num.length} numeros`)
+
+/*
+for( let pos in num){
+    console.log(` posicao ${pos} tem o valor ${num[pos]}`)
+}
+
+*/
+
---
+++ exercicios/exe003.js
@@ -0,0 +1,9 @@
+let valores = [1,2,3,4,5,6,7,8]
+
+let pos = valores.indexOf(2)
+
+if (pos == -1){
+    console.log(`o numero nao foi encontrado`)
+} else {
+    console.log(pos)
+} 
